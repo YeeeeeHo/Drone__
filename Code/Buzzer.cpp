c56441e8228ce9fb5cd7001856de93acb5855a65
예호 <139672321+YeeeeeHo@users.noreply.github.com>
2024-05-31 10:21:35 +0900

    Update Buzzer.cpp

--- Code/Buzzer.cpp
+++ Code/Buzzer.cpp
@@ -1,10 +1,11 @@
 #include <avr/io.h>
 
-void PWM_Init() {
+void buzzer_on(int a){
 	// PB7을 출력으로 설정
 	DDRB |= (1 << PB7);
 	// 타이머2를 Fast PWM 모드로 설정
 	TCCR2 |= (1 << WGM20) | (1 << WGM21);
+	
 	
 	// 비반전 모드 설정: COM21:0 = 2 -> OC2 클리어 시 비교 일치, BOTTOM에서 OC2 설정
 	TCCR2 |= (1 << COM21);
@@ -13,7 +14,11 @@ void PWM_Init() {
 	TCCR2 |= (1 << CS22);
 	
 	// PWM 듀티 사이클 설정 (0 ~ 255 사이 값으로 듀티 사이클 조절)
-	OCR2 = 128; // 예시로 50% 듀티 사이클로 설정
+	OCR2 = a; // 예시로 50% 듀티 사이클로 설정
+}
+void buzzer_off(){
+	// PWM 출력을 중지하기 위해, 비교 출력 모드를 0으로 설정
+	TCCR2 &= ~(1 << COM21);
 }
 int main(void) {
 	PWM_Init();
